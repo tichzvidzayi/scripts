b1f5d79cbb58640fd67b0c2e615ae94f2fc59317
Tich <tzvidzie@gmail.com>
2021-12-13 00:58:20 +0200

    v1

--- a.bash
+++ a.bash
@@ -1,7 +1,18 @@
 #!/bin/bash
-host=`hostname`
+HOST=`hostname`
+#HOST=$(hostname)
+CURRENT_DIRECTORY=$(pwd)
 echo Bonjour my server name is $host what is your name?
 echo 
-read name
+read -sp "Your name" NAME  #s hides user input
 echo
-echo Welcome $name 
+echo Welcome $NAME How many number do you want
+echo 
+read COUNT 
+i-100
+if [$COUNT -eq i]
+then
+   echo 
+else 
+    echo 
+fi        #End statement
